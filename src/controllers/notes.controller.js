c1d62b17cc0fbc4a61df6a49b4b044c0515646d9
CodeeHny <codee195@gmail.com>
2025-07-10 22:28:14 +0530

    Note : delete and update

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,6 +1,7 @@
 import { Notes } from "../models/notes.model.js";
 import { User } from "../models/user.model.js";
 
+
 const createNotes = async (req, res) => {
     try {
         const { title, content, isArchived } = req.body;
@@ -18,6 +19,9 @@ const createNotes = async (req, res) => {
 
         if (!notes) return res.status(400).json({ message: "Failed to create notes" });
 
+        user.notes.push(notes._id);
+        user.save({ validateBeforeSave: false });
+
         return res
             .status(200)
             .json({
@@ -25,16 +29,69 @@ const createNotes = async (req, res) => {
                 notes,
             })
     } catch (error) {
-        res.status(400).json({message: "Something went wrong while craeting notes"});
+        res.status(400).json({ message: "Something went wrong while craeting notes" });
         console.log("Error : ", error.message)
     }
+}
 
+const updateNotes = async (req, res) => {
+    try {
+        let { title, content, archived, collaborators } = req.body;
+
+        const updateObject = {};
+
+        if (title !== undefined && title.trim()) updateObject.title = title.trim();
+        if (content !== undefined && content.trim()) updateObject.content = content;
+        if (archived !== undefined && typeof archived !== 'boolean') updateObject.archived = archived;
+        if (collaborators !== undefined) updateObject.collaborators = collaborators;
+
+        let notes = await Notes.findOneAndUpdate(
+            { _id: req.params.notesId },
+            updateObject,
+            {
+                new: true
+            }
+        );
+        if (!notes) return res.status(404).json({ message: "Notes not found" });
+
+        return res.status(200).json({
+            message: "Notes updated successfully",
+            notes,
+        });
+
+    } catch (error) {
+        res.status(500).send("Something went wrong while updating notes");
+        console.log('Error : ', error.message);
+    }
+}
+
+const deleteNotes = async (req, res) => {
+    try {
+        let user = await User.findById(req.user._id);
+
+        if (!user.notes.includes(req.params.notesId)) return res.status(404).json({ message: "Notes not found " })
+
+        let notes = await Notes.findByIdAndDelete(req.params.notesId);
+
+        let notesIndex = user.notes.indexOf(req.params.notesId);
+        user.notes.splice(notesIndex, 1);
+        await user.save({ validateBeforeSave: false });
+
+        return res
+            .status(200)
+            .json({
+                message: "Notes deleted successfully",
+                deletedBlog: notes,
+                array: user.blogs
+            })
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong while deleting notes" });
+        console.log("Error: ", error.message);
+    }
 }
 
 export {
     createNotes,
+    updateNotes,
+    deleteNotes
 }
-
-// create -----
-// update 
-// delete 
--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { createNotes } from "../controllers/notes.controller.js";
+import { createNotes, deleteNotes, updateNotes } from "../controllers/notes.controller.js";
 
 const router = Router();
 
 router.route('/create').post(verifyJWT, createNotes);
+router.route('/update/:notesId').post(verifyJWT, updateNotes);
+router.route('/delete/:notesId').post(verifyJWT, deleteNotes);
 
 export default router; 
